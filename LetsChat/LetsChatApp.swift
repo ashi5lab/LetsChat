f5eefe94e047b7721e10779a20e8cc12a9872ade
ashi5lab <ashiqpkodiyathur@gmail.com>
2020-12-27 22:52:03 +0530

    Initial Commit

---
+++ LetsChat/LetsChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsChatApp.swift
+//  LetsChat
+//
+//  Created by Ashique on 27/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LetsChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
